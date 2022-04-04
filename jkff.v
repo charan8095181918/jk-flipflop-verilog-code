2ccb6ca764108761a1ac73f7b8fb1920ef7fe142
charan8095181918 <60030130+charan8095181918@users.noreply.github.com>
2022-04-04 13:08:31 +0530

    Add files via upload

---
+++ jkff.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:59:55 04/04/2022 
+// Design Name: 
+// Module Name:    jkff 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module jkff(input reset,input clk,input j,input k,output reg q,output qnot);
+assign qnot=~q;
+always @(posedge clk)
+	if(reset) q<=1'b0;else
+	case ({j,k})
+     2'b00: q<=q;
+     2'b01: q<=1'b0;
+     2'b10: q<=1'b1;
+     2'b11: q<=~q;
+	endcase
+endmodule 
---
+++ jkff_test.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   13:01:17 04/04/2022
+// Design Name:   jkff
+// Module Name:   D:/exp/dsd/Charan/jkff_test.v
+// Project Name:  Charan
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: jkff
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+module test;
+
+reg clk=0;
+reg j=0;
+reg k=0;
+reg reset=1;
+wire q,qnot;
+
+  jkff DUT (.reset(reset),.clk(clk),.j(j),.k(k),.q(q),.qnot(qnot));
+
+initial 
+   begin
+         $monitor($time,"j=%b k=%b Q=%b Qnot=%b",j,k,q,qnot);
+   j=1'b1;//set your j k here
+   k=1'b1;
+#5 reset=1'b0;
+#25 $finish;
+   end
+
+always #1 clk=~clk;
+
+endmodule
